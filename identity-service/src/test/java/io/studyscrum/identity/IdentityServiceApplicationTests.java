9aca39810eff1c189d598b60a9d7e282661afda9
rares4racu <rares4racu@gmail.com>
2026-05-24 11:55:13 +0300

    Fix: dezactiveaza context complet in teste CI

--- identity-service/src/test/java/io/studyscrum/identity/IdentityServiceApplicationTests.java
+++ identity-service/src/test/java/io/studyscrum/identity/IdentityServiceApplicationTests.java
@@ -2,12 +2,19 @@ package io.studyscrum.identity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.data.mongodb.uri=mongodb://localhost:27017/test",
+    "spring.security.oauth2.client.registration.google.client-id=test-client-id",
+    "spring.security.oauth2.client.registration.google.client-secret=test-client-secret",
+    "jwt.secret=test-jwt-secret-key-that-is-long-enough-for-testing-purposes",
+    "eureka.client.enabled=false",
+    "spring.cloud.discovery.enabled=false"
+})
 class IdentityServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
